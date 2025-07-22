08c4da4a9cb76a30c2b75a240231e96fbdc3cc3e
Pratham Raj <rjpratham04@gmail.com>
2025-07-22 16:41:35 +0530

    [Pratham Raj]. Add QuestionWrapper.java

---
+++ src/main/java/com/pratham/quizapp/model/QuestionWrapper.java
@@ -0,0 +1,19 @@
+package com.pratham.quizapp.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class QuestionWrapper {
+
+    private Integer id;
+    private String questionTitle;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+}
