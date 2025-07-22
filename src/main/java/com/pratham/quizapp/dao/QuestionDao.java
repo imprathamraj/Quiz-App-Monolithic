66f2c0a70f79c394471cb441e8808f0519a595a0
Pratham Raj <rjpratham04@gmail.com>
2025-07-22 16:39:42 +0530

    [Pratham Raj]. Add QuestionDao.java

---
+++ src/main/java/com/pratham/quizapp/dao/QuestionDao.java
@@ -0,0 +1,18 @@
+package com.pratham.quizapp.dao;
+
+import com.pratham.quizapp.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionDao extends JpaRepository<Question, Integer> {
+    List<Question> findByCategory(String category);
+
+    List<Question> findByDifficultyLevel(String difficultyLevel);
+
+    @Query(value = "SELECT * FROM question q Where q.category=:category ORDER BY RAND() LIMIT :numQ ", nativeQuery = true)
+    List<Question> findNRandomQuestionsByCategory(int numQ, String category);
+}
