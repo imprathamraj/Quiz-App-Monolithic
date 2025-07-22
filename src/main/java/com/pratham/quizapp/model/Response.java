7747edc29679af52566b07eb9a0f8fd36bb63c6b
Pratham Raj <rjpratham04@gmail.com>
2025-07-22 16:42:05 +0530

    [Pratham Raj]. Add Response.java

---
+++ src/main/java/com/pratham/quizapp/model/Response.java
@@ -0,0 +1,12 @@
+package com.pratham.quizapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Response {
+
+    private int id;
+    private String response;
+}
